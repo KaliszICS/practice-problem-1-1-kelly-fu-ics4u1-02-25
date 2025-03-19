4aac674c1781ad62e793389426a6d3a69e39cd5e
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-03-19 14:13:57 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,18 +3,18 @@ public class PracticeProblem {
 	public static int sum2D(int[][] array2D){
 		int sum =0;
 		for (int row = 0; row<array2D.length; row++){
-		for (int col =0; col<array2D[row].length; col++){
-		sum = sum+array2D[row][col];
+			for (int col =0; col<array2D[row].length; col++){
+				sum = sum+array2D[row][col];
+			}
 		}
+			return sum;
 	}
-		return sum;
-}
-
+	
 	public static int sumRow(int[][] arrTwo, int rowNum){
 		int sum = 0;
 		for (int x =0; x<arrTwo[rowNum].length; x++){
-		sum = sum+arrTwo[rowNum][x];
-	}
+			sum = sum+arrTwo[rowNum][x];
+		}
 		return sum;
 	}
 
@@ -23,9 +23,10 @@ public class PracticeProblem {
 		// row = y; col =x
 		int sum=0;
 		for (int y=0; y<arr2D.length; y++){
-		sum = sum + arr2D[y][colNum];
-	}
+			sum = sum + arr2D[y][colNum];
+		}
 		return sum;
-}
-
-}
+	}
+	
+	}
+	
